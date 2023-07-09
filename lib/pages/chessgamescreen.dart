86816005fdabdd4c79748da24c2ab3b2c9f9b26f
aartung <a.artun07@gmail.com>
2023-07-09 18:33:08 +0300

    fix

--- lib/pages/chessgamescreen.dart
+++ lib/pages/chessgamescreen.dart
@@ -182,6 +182,7 @@ class _ChessGameScreen extends State<ChessGameScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Column(
         children: [
           Expanded(
